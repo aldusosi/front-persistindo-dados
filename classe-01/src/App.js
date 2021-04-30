1518985be6193b5d8c772c5829cfcb07cf2440bd
Ernesto Júnior <ernesto.sjunior@hotmail.com>
2021-04-30 19:30:16 -0300

    classe-01

--- classe-01/src/App.js
+++ classe-01/src/App.js
@@ -1,8 +1,50 @@
 import './App.css';
+import { useState, useEffect, useRef } from 'react';
 
 function App() {
+  const [cronometro, setCronometro] = useState(0);
+  const [tema, setTema] = useState('claro');
+
+  const intervalID = useRef(null);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalID.current);
+    }
+
+  },[])
+
+  function handleComecar(){
+    intervalID.current = setInterval(() => setCronometro(tempoAnterior => tempoAnterior + 1), 1000);
+  }
+
+  function handlePausar(){
+    clearInterval(intervalID.current);
+  }
+
+  function handleFinalizar(){
+    setCronometro(0);
+    clearInterval(intervalID.current);
+
+  }
+
+  function handleReiniciar(){
+    setCronometro(0);
+  }
+
   return (
-    <h1>Crônometro</h1>
+    <div className={tema} id="App">
+      <button onClick={() => setTema( tema === 'claro' ? 'escuro' : 'claro')}>Trocar tema</button>
+      <h1>Crônometro</h1>
+      <span>{cronometro} segundos</span>
+      <div>
+        <button onClick={handleComecar}>Começar cronômetro</button>
+        <button onClick={handlePausar}>Pausar cronômetro</button>
+        <button onClick={handleFinalizar}>Finalizar cronômetro</button>
+        <button onClick={handleReiniciar}>Reiniciar cronômetro</button>
+        
+      </div>
+    </div>
   );
 }
 
